3df420777a22fb7fbe51464bf9363fb7444cc520
Jeremiah Jacob <jaykon4me@gmail.com>
2026-08-22 21:17:37 +0100

    test: add hex parsing and txid byte order unit tests

--- rfb_labs_week_3/serializeTrx/src/utils.rs
+++ rfb_labs_week_3/serializeTrx/src/utils.rs
@@ -25,3 +25,37 @@ pub fn txid_to_bytes(txid_str: &str) -> Result<Vec<u8>, String> {
 pub fn bytes_to_hex(bytes: &[u8]) -> String {
     hex::encode(bytes)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_hex_to_bytes_valid() {
+        assert_eq!(hex_to_bytes("0014a6").unwrap(), vec![0x00, 0x14, 0xa6]);
+        assert_eq!(hex_to_bytes("").unwrap(), Vec::<u8>::new());
+    }
+
+    #[test]
+    fn test_hex_to_bytes_odd_length() {
+        assert!(hex_to_bytes("0014a").is_err());
+    }
+
+    #[test]
+    fn test_hex_to_bytes_invalid_chars() {
+        assert!(hex_to_bytes("0014zz").is_err());
+    }
+
+    #[test]
+    fn test_txid_to_bytes_reversal() {
+        let display_txid = "21c80d2b05c1106360a36e435ad8e418e85d0eb708d9f2bf216476b37bd0b08f";
+        let wire_bytes = txid_to_bytes(display_txid).unwrap();
+        let expected_wire_hex = "8fb0d07bb3766421bff2d908b70e5de818e4d85a436ea3606310c1052b0dc821";
+        assert_eq!(bytes_to_hex(&wire_bytes), expected_wire_hex);
+    }
+
+    #[test]
+    fn test_txid_to_bytes_invalid_length() {
+        assert!(txid_to_bytes("0014a6").is_err());
+    }
+}
